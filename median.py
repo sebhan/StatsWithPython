b3404f1413409ef9065ae1dc659ee79542a25f86
Sebastian Hansen <sebastian@fromcodetoclinic.com>
2018-08-06 14:19:49 +0200

    Update median.py

--- median.py
+++ median.py
@@ -2,21 +2,22 @@ import random
 
 array = []
 
-for i in range(500):
+for i in range(5):
     array.append(random.random())
 
-arrayLength = len(array)
-arrayHalf = arrayLength/2.0
 array = sorted(array)
 
-if arrayLength % 2 == 0:
-    arrayHalf = int(arrayHalf)
-    arrayMedian = (array[arrayHalf] + array[arrayHalf-1])/2.0
-else:
-    arrayHalf = arrayHalf-0.5
-    arrayMedian = array[arrayHalf]
-    
-print array,"\n"
-print "Median:", arrayMedian
+def median(array):
+    arrayLength = len(array)
+    arrayHalf = arrayLength/2.0
+    if arrayLength % 2 == 0:
+        arrayHalf = int(arrayHalf)
+        arrayMedian = (array[arrayHalf] + array[arrayHalf-1])/2.0
+    else:
+        arrayHalf = int(arrayHalf-0.5)
+        arrayMedian = array[arrayHalf]
+    return arrayMedian
+
+print "Median:", median(array)
 print "Minimum:", min(array)
 print "Maximum:", max(array)
