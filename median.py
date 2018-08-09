54567434ae2ea5a79ef03cb4faf9f1f586ed8747
Sebastian Hansen <sebastian@fromcodetoclinic.com>
2018-08-09 11:34:51 +0200

    Update median.py
    Added calculation of the mean.

--- median.py
+++ median.py
@@ -1,7 +1,7 @@
 import random
 
 array = []
-size = 500
+size = 51
 
 for i in range(size):
     array.append(random.random())
@@ -28,8 +28,11 @@ upperHalf = array[arrayHalf:size]
 firstQuartile = median(lowerHalf)[0]
 thirdQuartile = median(upperHalf)[0]
 
+arrayMean = sum(array)/size
+
 print "Minimum:", "\t", min(array)
 print "First quartile:", firstQuartile
 print "Median:", "\t", arrayMedian
+print "Mean", "\t\t", arrayMean
 print "Third quartile:", thirdQuartile
 print "Maximum:", "\t", max(array)
